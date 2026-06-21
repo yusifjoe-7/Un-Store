8c88bd3013140f8b3117167abd39f6548246360c
yusifjoe7 <yusifjoe7@gmail.com>
2026-06-21 08:29:15 +0300

    add back arow

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -188,6 +188,12 @@ const handleAdd = async () => {
           <span className="text-foreground truncate max-w-[200px]">{product.title}</span>
         </nav> */}
 
+        <div className="mt-2 mb-2 md:hidden ml-2 text-muted-foreground"
+        onClick={()=>router.back()}
+        >
+          <svg width="23" height="23" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+        </div>
+
         {/* Main Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
 
